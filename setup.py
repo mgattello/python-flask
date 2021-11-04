c1b279b061304da00060f6c9e8b7f53102679e3f
mgattello <markgatte@gmail.com>
2021-11-04 09:50:05 +0000

    add more info in setup, create pyproject with max length of 79 following  PEP8

--- setup.py
+++ setup.py
@@ -4,5 +4,16 @@ from setuptools import find_packages
 setup(
     name="python-flask",
     version="0.0.1",
-    description="API with Python and Flask"
+    description="API with Python and Flask",
+    author="Marco Gattello",
+    author_email="mgattello@gmail.com",
+    url="https://github.com/mgattello/python-flusk",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages= find_packages(where="src"),
+    python_requires=">=3.6",
 )
