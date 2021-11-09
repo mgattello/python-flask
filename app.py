7d8d9640bbc2e76d6364ed3d856505001765ac9f
mgattello <markgatte@gmail.com>
2021-11-09 16:45:29 +0000

    add post method in Article and tests.

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
-api = Api(app)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///' + os.path.join(basedir, 'db.sqlite')
+# Silence the FSADeprecationWarning and save resource
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']='False'
 
-class HelloWorld(Resource):
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class ArticleModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True)   
+    body = db.Column(db.String)
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+class ArticleSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = ArticleModel
+        fields = ("id", "title", "body")
+
+article_schema = ArticleSchema()
+articles_schema = ArticleSchema(many=True)
+
+class HelloWorld():
     def get(self):
         return 'Hello World!'
 
-class Article(Resource):
+class Article():
     def __init__(self, db = []):
         self.db = db
 
@@ -26,11 +51,34 @@ class Article(Resource):
             return split_result
         return self.db
 
+    def post(self, article_title, article_body):
+        # TODO: exe. article id taken 409
+        new_article = {
+            'title': article_title,
+            'body': article_body
+        }
+
+        return new_article
+
     def errorMessage(self):
         return {'error': 'not found'}
 
-api.add_resource(HelloWorld, '/')
-api.add_resource(Article, '/articles')
+@app.route('/', methods=['GET'])
+def hello_world():
+    res = {"hello": "world"}
+    return res
+
+@app.route('/article', methods=['POST'])
+def add_article():
+    title = request.json['title']
+    body = request.json['body']
+
+    new_article = ArticleModel(title, body)
+    
+    db.session.add(new_article)
+    db.session.commit()
+
+    return article_schema.dumps(new_article)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test/test_article.py
+++ test/test_article.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import MagicMock, Mock
+from unittest.mock import MagicMock
 from app import Article
 
 class TestArticle(unittest.TestCase):
@@ -30,19 +30,14 @@ class TestArticle(unittest.TestCase):
         # articles length > 10
         limit_articles_number = 10
         articles = ['test', 'test', 'test', 412, 12, 12, 1, {}, [], 'test', 'test', 'test']
-        setup_article = self.setupArticle(articles)
+        result_length = len(Article(articles).getAll())
 
-        get_all_result = setup_article.getAll()
-        result_length = len(get_all_result)
-        
         self.assertEqual(result_length, limit_articles_number)
 
     def test_get_all_articles_length_minor_10(self):
         setup_article = self.setupArticle(self.output_expected)
         articles_length = len(self.output_expected)
-        
-        get_all_result = setup_article.getAll()
-        result_length = len(get_all_result)
+        result_length = len(setup_article.getAll())
         
         self.assertEqual(result_length, articles_length)
 
@@ -60,13 +55,12 @@ class TestArticle(unittest.TestCase):
         self.assertEqual(output, expected)
 
     def test_errormessage_tobe_called(self):
-        self.setup_empty_articles.errorMessage = MagicMock()
-        articles = self.setup_empty_articles.get()
-        outside_articles_length = len(articles) + 1
+        self.setup_articles.errorMessage = MagicMock()
+        outside_articles_length = len(self.setup_articles.get()) + 1
 
-        self.setup_empty_articles.get(outside_articles_length)
+        self.setup_articles.get(outside_articles_length)
 
-        self.setup_empty_articles.errorMessage.assert_called_once()
+        self.setup_articles.errorMessage.assert_called_once()
 
     def test_errormessage_not_tobe_called(self):
         self.setup_articles.errorMessage = MagicMock()
@@ -74,6 +68,20 @@ class TestArticle(unittest.TestCase):
         self.setup_articles.get(1)
         
         self.setup_articles.errorMessage.assert_not_called()
+    
+    def test_post_article(self):
+        article_id = 1
+        article_title = 'Test'
+        article_body = 'this is my article'
+        expected = {
+            'id': 1,
+            'title': 'Test',
+            'body': 'this is my article'
+        }
+
+        output = self.setup_articles.post(article_id, article_title, article_body)
+
+        self.assertEqual(output, expected)
 
 if __name__ == '__main__':
     unittest.main()
